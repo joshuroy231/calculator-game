4e1ac0c5ee06a87032556ead7fc172ff3566431d
Josh Palmer <joshuabrent05@gmail.com>
2026-05-23 14:36:51 -0600

    Refactor collision code

--- src/systems/physics-system.cpp
+++ src/systems/physics-system.cpp
@@ -6,54 +6,40 @@ PhysicsSystem::PhysicsSystem(Entity* entity_registry, int const& num_entities, T
 {}
 
 void resolveXCollision(Entity& entity, Tilemap* tilemap) {
-    int top_index = entity.position.y / TILE_PIXELS;
+    if (entity.velocity.x == 0) return;
+    int top_row = entity.position.y / TILE_PIXELS;
     int bottom_coord = entity.position.y + entity.entity_profile->dimensions.h;
-    int bottom_index = (bottom_coord % TILE_PIXELS == 0) ? (bottom_coord / TILE_PIXELS - 1) : (bottom_coord / TILE_PIXELS);
-    if (entity.velocity.x > 0) {
-        int right_index = (entity.position.x + entity.entity_profile->dimensions.w) / TILE_PIXELS;
-        for (int i = top_index; i <= bottom_index; i++) {
-            if (tilemap->data[i*tilemap->num_cols + right_index] != 0) {
-                entity.position.x -= entity.position.x % TILE_PIXELS;
-                entity.velocity.x = 0;
-                break;
-            }
-        }
-    }
-    if (entity.velocity.x < 0) {
-        int left_index = (entity.position.x) / TILE_PIXELS;
-        for (int i = top_index; i <= bottom_index; i++) {
-            if (tilemap->data[i*tilemap->num_cols + left_index] != 0) {
-                entity.position.x += TILE_PIXELS;
-                entity.position.x -= entity.position.x % TILE_PIXELS;
-                entity.velocity.x = 0;
-                break;
-            }
+    int bottom_row = (bottom_coord % TILE_PIXELS == 0) ? (bottom_coord / TILE_PIXELS - 1) : (bottom_coord / TILE_PIXELS);
+    bool moving_right = (entity.velocity.x > 0);
+    int column = (moving_right)
+        ? (entity.position.x + entity.entity_profile->dimensions.w) / TILE_PIXELS
+        : (entity.position.x) / TILE_PIXELS;
+    for (int i = top_row; i <= bottom_row; i++) {
+        if (tilemap->data[i*tilemap->num_cols + column] != 0) {
+            entity.position.x += (moving_right)
+                ? -entity.position.x%TILE_PIXELS
+                : TILE_PIXELS - entity.position.x%TILE_PIXELS;
+            entity.velocity.x = 0;
+            break;
         }
     }
 }
 void resolveYCollision(Entity& entity, Tilemap* tilemap) {
+    if (entity.velocity.y == 0) return;
     int left_index = entity.position.x / TILE_PIXELS;
     int right_coord = entity.position.x + entity.entity_profile->dimensions.w;
     int right_index = (right_coord % TILE_PIXELS == 0) ? (right_coord / TILE_PIXELS - 1) : (right_coord / TILE_PIXELS);
-    if (entity.velocity.y > 0) {
-        int bottom_index = (entity.position.y + entity.entity_profile->dimensions.h) / TILE_PIXELS;
-        for (int i = left_index; i <= right_index; i++) {
-            if (tilemap->data[bottom_index*tilemap->num_cols + i] != 0) {
-                entity.position.y -= entity.position.y % TILE_PIXELS;
-                entity.velocity.y = 0;
-                break;
-            }
-        }
-    }
-    if (entity.velocity.y < 0) {
-        int top_index = (entity.position.y) / TILE_PIXELS;
-        for (int i = left_index; i <= right_index; i++) {
-            if (tilemap->data[top_index*tilemap->num_cols + i] != 0) {
-                entity.position.y += TILE_PIXELS;
-                entity.position.y -= entity.position.y % TILE_PIXELS;
-                entity.velocity.y = 0;
-                break;
-            }
+    bool moving_down = (entity.velocity.y > 0);
+    int row = (moving_down)
+        ? (entity.position.y + entity.entity_profile->dimensions.h) / TILE_PIXELS
+        : (entity.position.y) / TILE_PIXELS;
+    for (int i = left_index; i <= right_index; i++) {
+        if (tilemap->data[row*tilemap->num_cols + i] != 0) {
+            entity.position.y += (moving_down)
+                ? -entity.position.y%TILE_PIXELS
+                : TILE_PIXELS - entity.position.y % TILE_PIXELS;
+            entity.velocity.y = 0;
+            break;
         }
     }
 }
