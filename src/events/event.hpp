12c05ba28b2c94868f5645c433e463f99fc13456
Josh Palmer <joshuabrent05@gmail.com>
2026-06-02 16:33:19 -0600

    Add basic event types

--- src/events/event.hpp
+++ src/events/event.hpp
@@ -1,16 +1,30 @@
 #pragma once
 
+#include "entities/entity-profile.hpp"
+
 enum class EventType {
-    TILE_COLLISION,
-    ENTITY_COLLISION,
+    CONDEMN_ENTITY,
+    CONCEIVE_ENTITY,
+};
+
+struct CondemnEntityEvent {
+    int entity_id;
+    CondemnEntityEvent(int entity_id)
+    : entity_id(entity_id) {}
+};
+struct ConceiveEntityEvent {
+    EntityConfiguration entity_configuration;
+    ConceiveEntityEvent(const EntityConfiguration& entity_configuration)
+    : entity_configuration(entity_configuration) {}
+};
+
+union EventData {
+    CondemnEntityEvent condemn_entity;
+    ConceiveEntityEvent conceive_entity;
 };
 
 class Event {
     public:
         EventType event_type;
         EventData event_data;
-};
-
-union EventData {
-
 };
