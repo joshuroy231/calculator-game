6af9381a3ddfc4353d74866c551311f57dd6f69a
Josh Palmer <joshuabrent05@gmail.com>
2026-06-02 16:57:14 -0600

    Event stuff

--- src/events/event.hpp
+++ src/events/event.hpp
@@ -21,10 +21,27 @@ struct ConceiveEntityEvent {
 union EventData {
     CondemnEntityEvent condemn_entity;
     ConceiveEntityEvent conceive_entity;
+
+    EventData(CondemnEntityEvent event)
+    : condemn_entity(event) {}
+    EventData(ConceiveEntityEvent event)
+    : conceive_entity(event) {}
 };
 
 class Event {
-    public:
+    private:
         EventType event_type;
+    public:
         EventData event_data;
+
+        Event(CondemnEntityEvent event)
+        : event_type(EventType::CONDEMN_ENTITY)
+        , event_data(event) {}
+        Event(CondemnEntityEvent event)
+        : event_type(EventType::CONCEIVE_ENTITY)
+        , event_data(event) {}
+
+        EventType getEventType() {
+            return event_type;
+        }
 };
