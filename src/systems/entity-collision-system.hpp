79493b9db295cd4a5fefeaeeb3c42f78abeec95e
Josh Palmer <joshuabrent05@gmail.com>
2026-06-04 20:37:17 -0600

    Inject collision_matrix into entity collision system

--- src/systems/entity-collision-system.hpp
+++ src/systems/entity-collision-system.hpp
@@ -3,11 +3,20 @@
 #include "systems/system.hpp"
 #include "events/event.hpp"
 #include "utilities/queue.hpp"
+#include "on-collision-function.hpp"
+#include "utilities/matrix.hpp"
 
 
 class EntityCollisionSystem : public System {
+    private:
+        Matrix<OnCollisionFunction>& collision_matrix;
     public:
-        EntityCollisionSystem(Entity* entity_registry, const int& num_entities)
-        : System(entity_registry, num_entities) {}
+        EntityCollisionSystem(
+            Entity* entity_registry,
+            Matrix<OnCollisionFunction>& collision_matrix,
+            const int& num_entities
+        )
+        : System(entity_registry, num_entities)
+        , collision_matrix(collision_matrix) {}
         void update() override;
 };
