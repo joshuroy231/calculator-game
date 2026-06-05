55ac39713e44c5d4bce026bd2b9bfd7a21f27806
Josh Palmer <joshuabrent05@gmail.com>
2026-06-04 20:13:04 -0600

    Add handy matrix class that stores a flattened matrix and allows dual indexing

---
+++ src/utilities/matrix.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+template<typename T>
+class MatrixRow {
+    private:
+        T* data;
+    public:
+        MatrixRow(T* data)
+        : data(data) {}
+        T& operator[](int i) {
+            return data[i];
+        }
+        const T& operator[](int i) const {
+            return data[i];
+        }
+};
+
+template<typename T>
+class Matrix {
+    private:
+        int width;
+        int height;
+        T* data;
+    public:
+        Matrix(int width, int height) {
+            this->width = width;
+            this->height = height;
+            data = new T[width*height];
+        }
+        MatrixRow<T> operator[](int i) {
+            return MatrixRow<T>(data + i*width);
+        }
+        const MatrixRow<T> operator[](int i) const {
+            return MatrixRow<T>(data + i*width);
+        }
+};
