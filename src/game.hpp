9061952047811acccf7d080d51d96cecbbefbe27
Josh Palmer <joshuabrent05@gmail.com>
2026-06-04 20:28:38 -0600

    Inject collision_matrix into the game object

--- src/game.hpp
+++ src/game.hpp
@@ -1,18 +1,23 @@
 #pragma once
 
 #include "scene.hpp"
+#include "on-collision-function.hpp"
+#include "utilities/matrix.hpp"
 
 class Game {
     public:
         Scene* scene;
         EntityProfile* entity_profiles;
+        Matrix<OnCollisionFunction>& collision_matrix;
         int entity_pool_size;
         Game(
             Scene* scene,
             EntityProfile* entity_profiles,
+            Matrix<OnCollisionFunction>& collision_matrix,
             int entity_pool_size
         )
         : scene(scene)
         , entity_profiles(entity_profiles)
+        , collision_matrix(collision_matrix)
         , entity_pool_size(entity_pool_size) {}
 };
--- src/games/test-game/game-impl.cpp
+++ src/games/test-game/game-impl.cpp
@@ -1,12 +1,14 @@
 #include "games/test-game/game-impl.hpp"
 #include "games/test-game/scenes.hpp"
 #include "games/test-game/definitions/entity-profiles.hpp"
+#include "games/test-game/definitions/on-collision-functions.hpp"
 
 namespace Games {
     Game* get() {
         static Game* test_game = new Game(
             Scenes::get(),
             EntityProfiles::get(),
+            OnCollisionFunctions::get(),
             32
         );
         return test_game;
