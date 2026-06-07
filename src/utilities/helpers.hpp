a9e7fac2246e55026d46b48e9301ed7e343d5855
Josh Palmer <joshuabrent05@gmail.com>
2026-06-07 13:34:43 -0600

    Tweak getCollision function

--- src/utilities/helpers.hpp
+++ src/utilities/helpers.hpp
@@ -37,12 +37,11 @@ Vec2<int> getCollision(Box<int> box1, Vec2<int> v1, Box<int> box2, Vec2<int> v2)
     int x_weight = abs(overlap.y * relative_velocity.x);
     int y_weight = abs(overlap.x * relative_velocity.y);
 
-    if (y_weight > x_weight) {
-        if (overlap.y*relative_velocity.y < 0) return Vec2(0,0);
+    if (y_weight > x_weight && overlap.y*relative_velocity.y >= 0) {
         return Vec2(0, overlap.y);
     }
-    else {
-        if (overlap.x*relative_velocity.x < 0) return Vec2(0,0);
+    else if (overlap.x*relative_velocity.x >= 0) {
         return Vec2(overlap.x, 0);
     }
+    else return Vec2<int>(0,0);
 }
