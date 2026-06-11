95660ef6aff5604258405204e7d9e20c0c495630
Josh Palmer <joshuabrent05@gmail.com>
2026-06-11 10:31:07 -0600

    Change system order in the array, which changes the order in which they are updated

--- src/game-engine.cpp
+++ src/game-engine.cpp
@@ -13,13 +13,10 @@ GameEngine::GameEngine(Game* game)
     )
     , game(game)
     {
-        systems[0] = new RenderingSystem(
+        systems[0] = new ControlSystem(
             entity_manager.entity_registry,
             entity_manager.num_entities,
-            event_queue,
-            &game->scene->tilemap,
-            game->scene->background_color,
-            game->scene->camera.position
+            event_queue
         );
         systems[1] = new PhysicsSystem(
             entity_manager.entity_registry,
@@ -28,23 +25,26 @@ GameEngine::GameEngine(Game* game)
             &game->scene->tilemap,
             game->scene->gravity
         );
-        systems[2] = new ControlSystem(
-            entity_manager.entity_registry,
-            entity_manager.num_entities,
-            event_queue
-        );
-        systems[3] = new EntityCollisionSystem(
+        systems[2] = new EntityCollisionSystem(
             entity_manager.entity_registry,
             entity_manager.num_entities,
             event_queue,
             game->collision_matrix
         );
-        systems[4] = new CameraSystem(
+        systems[3] = new CameraSystem(
             entity_manager.entity_registry,
             entity_manager.num_entities,
             event_queue,
             game->scene->camera,
             game->scene->dimensions_pixels
+        );
+        systems[4] = new RenderingSystem(
+            entity_manager.entity_registry,
+            entity_manager.num_entities,
+            event_queue,
+            &game->scene->tilemap,
+            game->scene->background_color,
+            game->scene->camera.position
         );
     }
 
