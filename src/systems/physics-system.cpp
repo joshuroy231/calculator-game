a8a60bd3c4d7f97862e09d839e3df6ed5166b429
Josh Palmer <joshuabrent05@gmail.com>
2026-06-10 18:06:06 -0600

    Revert physics system code to how it was before because I couldn't make the other way work for now. If I want to try again in the future, I will create a branch

--- src/systems/physics-system.cpp
+++ src/systems/physics-system.cpp
@@ -1,7 +1,5 @@
 #include "physics-system.hpp"
 
-#include "utilities/helpers.hpp"
-
 PhysicsSystem::PhysicsSystem(
     Entity* entity_registry,
     int const& num_entities,
@@ -14,42 +12,102 @@ PhysicsSystem::PhysicsSystem(
 , gravity(gravity)
 {}
 
+void resolveXCollision(Entity& entity, bool moving_right) {
+    entity.state.position.x += (moving_right)
+        ? -(entity.state.position.x + entity.profile->dimensions.x)%TILE_PIXELS
+        : TILE_PIXELS - entity.state.position.x%TILE_PIXELS;
+    entity.state.velocity.x = 0;
+}
+void checkXCollision(Entity& entity, Tilemap* tilemap) {
+    if (entity.state.velocity.x == 0) return;
+    // check left boundary collision
+    if (entity.state.position.x < 0) {
+        entity.state.position.x = 0;
+        entity.state.velocity.x = 0;
+        return;
+    }
+    // check right boundary collision
+    if (entity.state.position.x + entity.profile->dimensions.x > tilemap->tile_dimensions.x*TILE_PIXELS) {
+        entity.state.position.x = tilemap->tile_dimensions.x*TILE_PIXELS - entity.profile->dimensions.x;
+        entity.state.velocity.x = 0;
+        return;
+    }
+    int top_row = entity.state.position.y / TILE_PIXELS;
+    int bottom_coord = entity.state.position.y + entity.profile->dimensions.y;
+    int bottom_row = (bottom_coord % TILE_PIXELS == 0) ? (bottom_coord / TILE_PIXELS - 1) : (bottom_coord / TILE_PIXELS);
+    bool moving_right = (entity.state.velocity.x > 0);
+    int column = (moving_right)
+        ? (entity.state.position.x + entity.profile->dimensions.x) / TILE_PIXELS
+        : (entity.state.position.x) / TILE_PIXELS;
+    for (int i = top_row; i <= bottom_row; i++) {
+        if (tilemap->data[i*tilemap->tile_dimensions.x + column] != 0) {
+            resolveXCollision(entity, moving_right);
+            break;
+        }
+    }
+}
+
+void resolveYCollision(Entity& entity, bool moving_down) {
+    entity.state.position.y += (moving_down)
+        ? -(entity.state.position.y + entity.profile->dimensions.y)%TILE_PIXELS
+        : TILE_PIXELS - entity.state.position.y % TILE_PIXELS;
+    entity.state.velocity.y = 0;
+}
+void checkYCollision(Entity& entity, Tilemap* tilemap) {
+    if (entity.state.velocity.y == 0) return;
+    // Resolve top collision
+    if (entity.state.position.y < 0) {
+        entity.state.position.y = 0;
+        entity.state.velocity.y = 0;
+        return;
+    }
+    // resolve bottom collision
+    if (entity.state.position.y + entity.profile->dimensions.y > tilemap->tile_dimensions.y*TILE_PIXELS) {
+        entity.state.position.y = tilemap->tile_dimensions.y*TILE_PIXELS - entity.profile->dimensions.y;
+        return;
+    }
+    int left_index = entity.state.position.x / TILE_PIXELS;
+    int right_coord = entity.state.position.x + entity.profile->dimensions.x;
+    int right_index = (right_coord % TILE_PIXELS == 0) ? (right_coord / TILE_PIXELS - 1) : (right_coord / TILE_PIXELS);
+    bool moving_down = (entity.state.velocity.y > 0);
+    int row = (moving_down)
+        ? (entity.state.position.y + entity.profile->dimensions.y) / TILE_PIXELS
+        : (entity.state.position.y) / TILE_PIXELS;
+    for (int i = left_index; i <= right_index; i++) {
+        if (tilemap->data[row*tilemap->tile_dimensions.x + i] != 0) {
+            resolveYCollision(entity, moving_down);
+            break;
+        }
+    }
+}
+
 void PhysicsSystem::update() {
     for (int i = 0; i < this->num_entities; i++) {
         if (!entity_registry[i].profile->has_physics) continue;
         Entity& entity = this->entity_registry[i];
-        Vec2<int>& p = entity.state.position;
-        Vec2<int>& v = entity.state.velocity;
 
-        v += this->gravity/FPS;
-        v += entity.state.acceleration/FPS;
+        entity.state.velocity += this->gravity/FPS;
+        entity.state.velocity += entity.state.acceleration/FPS;
 
-        if (v.x != 0) {
-            int decay = v.x / 5;
+        int& v = entity.state.velocity.x;
+        if (v != 0) {
+            int decay = v / 5;
 
-            if (decay != 0) v.x -= decay;
-            else v.x -= (v.x > 0) ? 1 : -1;
-        }
-
-        if (v.x > entity.profile->terminal_velocity_x) v.x = entity.profile->terminal_velocity_x;
-        else if (v.x < -entity.profile->terminal_velocity_x) v.x = -entity.profile->terminal_velocity_x;
-        if (v.y > entity.profile->terminal_velocity_y) v.y = entity.profile->terminal_velocity_y;
-        else if (v.y < -entity.profile->terminal_velocity_y) v.y = -entity.profile->terminal_velocity_y;
-
-        p += v / FPS;
-
-        Box<int> entity_box = Box<int>(p, entity.profile->dimensions);
-        for (int i = entity_box.x_min / TILE_PIXELS; i <= entity_box.x_max / TILE_PIXELS; i++) {
-            for (int j = entity_box.y_min / TILE_PIXELS; j <= entity_box.y_max / TILE_PIXELS; j++) {
-                Box<int> tile_box = Box<int>(i*TILE_PIXELS, j*TILE_PIXELS, (i+1)*TILE_PIXELS, (j+1)*TILE_PIXELS);
-                if (tilemap->data[j*tilemap->tile_dimensions.x + i] == 0) continue;
-                Vec2<int> collision = getCollision(entity_box, v, tile_box, Vec2<int>(0, 0));
-                p += collision;
-                if (collision.x != 0) v.x = 0;
-                if (collision.y != 0) v.y = 0;
-                goto collision_resolved;
+            if (decay != 0) {
+                v -= decay;
+            } else {
+                v -= (v > 0) ? 1 : -1;
             }
         }
-        collision_resolved:;
+
+        if (entity.state.velocity.x > entity.profile->terminal_velocity_x) entity.state.velocity.x = entity.profile->terminal_velocity_x;
+        else if (entity.state.velocity.x < -entity.profile->terminal_velocity_x) entity.state.velocity.x = -entity.profile->terminal_velocity_x;
+        if (entity.state.velocity.y > entity.profile->terminal_velocity_y) entity.state.velocity.y = entity.profile->terminal_velocity_y;
+        else if (entity.state.velocity.y < -entity.profile->terminal_velocity_y) entity.state.velocity.y = -entity.profile->terminal_velocity_y;
+
+        entity.state.position.x += entity.state.velocity.x / FPS;
+        checkXCollision(entity, tilemap);
+        entity.state.position.y += entity.state.velocity.y / FPS;
+        checkYCollision(entity, tilemap);
     }
 }
